41f6107f3340041092a125eb89573c53f1ddb91c
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2026-04-14 18:03:16 -0300

    DB Connection Test

---
+++ Servidor/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import pg from 'pg'
+const { Client } = pg
+
+const client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: process.env.DB_PASSWORD,
+    database: 'itam'
+})
+
+client.connect()
+
+client.query(`SELECT * FROM  equipamentos`, (err, res) => {
+    if (!err){
+        console.log(res.rows);
+
+    }else{
+        console.log(err.message);
+    }
+})
